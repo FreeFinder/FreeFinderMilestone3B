67b907b6cbefc68dfb093c42bb5217dd67a1a7fa
jorlab1 <jlabuda@uchicago.edu>
2022-11-02 18:33:25 -0500

    Initial Commit

---
+++ FreeFinderMilestone3B/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FreeFinderMilestone3B
+//
+//  Created by Jordan Labuda on 11/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreeFinderMilestone3B/FreeFinderMilestone3BApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeFinderMilestone3BApp.swift
+//  FreeFinderMilestone3B
+//
+//  Created by Jordan Labuda on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FreeFinderMilestone3BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
