5bfa209dd89da2886bdea5da1db3b7d27b54aa1b
wzeng0 <wzeng00@uchicago.edu>
2022-11-06 18:21:21 -0600

    started item_annotation

--- FreeFinderMilestone3B/FreeFinderMilestone3BApp.swift
+++ FreeFinderMilestone3B/FreeFinderMilestone3BApp.swift
@@ -6,6 +6,31 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
+import FirebaseAuth
+
+func item_to_annot(item_id: String){
+    var ref = Database.database(url: "https://freefinder-12f0c-default-rtdb.firebaseio.com/").reference()
+    var itemInfo = ref.child("items/\(item_id)").getData(completion:  { error, snapshot in
+      guard error == nil else {
+        print(error!.localizedDescription)
+        return;
+      }
+      let userName = snapshot.value as? String ?? "Unknown";
+    });
+    let json = try? JSONSerialization.jsonObject(with: itemInfo, options: [])
+    
+}
+
+class Item {
+    var id : String
+    var title : String
+    var description : String
+    var photo : [[Int]]
+    var quantity : Int
+    var comments : [String]
+}
 
 @main
 struct FreeFinderMilestone3BApp: App {
